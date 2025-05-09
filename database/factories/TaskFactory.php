5225c41506c269fcec436e04dceb1aad5d24097d
eneko-izt <eneko@izt.coop>
2025-05-09 09:55:55 +0200

    Set a default random value for order column

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -5,13 +5,13 @@
 use App\Task;
 use Faker\Generator as Faker;
 
-//TODO order eremua falta da
 $factory->define(Task::class, function (Faker $faker) {
     return [
         'user_id' => App\User::inRandomOrder()->first()->id,
         'column_id' => App\Column::inRandomOrder()->first()->id,
         'text' => $faker->sentence,
         'active' => $faker->boolean,
+        'order' => $faker->randomNumber(),
         'created_at' => now(),
         'updated_at' => now()
     ];
