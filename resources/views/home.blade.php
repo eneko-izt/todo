ccbce65874dfbe1a9512f1810ceb045741a5be5a
eneko-izt <eneko@izt.coop>
2025-05-29 14:16:10 +0200

    Desiplay columns  in dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,34 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+<h3>{{ Date('Y-m-d') }}</h3>
+<a href="{{ route('welcome') }}" class="btn btn-primary">Home</a>
+<a href="{{ route('home') }}" class="btn btn-primary">Go to Dashboard</a>
+<a href="{{ route('columns.index') }}" class="btn btn-primary">Columns</a>
+<a href="{{ route('tags.index') }}" class="btn btn-primary">Tags</a>
 
-                    <div class="row">
-                        <div class="col-md-10">
-                            <h3>{{ Date('Y-m-d') }}</h3>
-                        </div>
-                        <div class="col-md-2 text-right">
-                            <a href="{{ route('welcome') }}" class="btn btn-primary">Home</a>
-                            <a href="{{ route('home') }}" class="btn btn-primary">Go to
-                                Dashboard</a>
-                            <a href="{{ route('columns.index') }}"
-                                class="btn btn-primary">Columns</a>
-                            <a href="{{ route('tags.index') }}"
-                                class="btn btn-primary">Tags</a>
-                        </div>
-                    </div>
-                </div>
+<div class="d-flex justify-content-between mt-5">
+
+    <div class="d-flex justify-content-between w-100">    
+        @forelse ($columns as $column)
+
+            <div class="p-2 text-white @if (!$loop->first) ml-2 @endif"
+                style="width: {{ 100 / $columns->count() }}%; height:150px; background-color: {{ $column->colour }};">
+                {{ $column->name }}
             </div>
-        </div>
+
+        @empty
+            <p>No columns found.</p>
+        @endforelse
     </div>
+   
+</div>
+
 @endsection
