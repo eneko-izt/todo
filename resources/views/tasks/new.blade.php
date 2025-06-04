a03a4cc4e8dca9d78b48bf8d89e73e2d5889d1ff
eneko-izt <eneko@izt.coop>
2025-06-04 10:59:35 +0200

    Show validation errors

--- resources/views/tasks/new.blade.php
+++ resources/views/tasks/new.blade.php
@@ -3,23 +3,35 @@
         @csrf
         <div>
             <textarea 
-                class="form-control mb-2" 
+                class="form-control mb-2 @error('name') help is-danger @enderror" 
                 name="text" 
-                id="text" 
-                rows="3" 
+                id="text"
+                rows="3"
                 maxlength="255"
-                space
-                required></textarea>
-            <label for="fname">Order:</label>
+                style="@error('text') color:#d8000c @enderror"
+                required
+            >{{ old('text') }}</textarea>
+
+            @error('text')
+                <p class="help is-danger" style="color:#d8000c">{{ $errors->first('text') }}</p>
+            @enderror
+
+            <label for="input">Order:</label>
             <input 
                 type="number" 
-                class="form-control mb-2" 
+                class="form-control mb-2 @error('order') help is-danger @enderror" 
                 name="order" 
                 id="order" 
+                value="{{ old('order') }}"
                 min="0"
                 max="100"
+                style="@error('order') color:#d8000c @enderror"
                 required
             >
+            @error('order')
+                <p class="help is-danger" style="color:#d8000c">{{ $errors->first('order') }}</p>
+            @enderror
+
             <input 
                 type="hidden" 
                 name="column_id" 
