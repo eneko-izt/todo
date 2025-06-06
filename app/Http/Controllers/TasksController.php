9e77fe70c833ffc436599e2fe818c626d1def64f
eneko-izt <eneko@izt.coop>
2025-06-06 10:35:55 +0200

    Added custom validation for user and tags (required and exist  in tables)

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -6,6 +6,8 @@ use App\Task;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 class TasksController extends Controller
 {
@@ -31,18 +33,21 @@ class TasksController extends Controller
 
         $attributes = $this->validateTaskCreate(request('column_id'));
 
+        $data = ['user_id' => auth()->id()];
+        $validator =Validator::make($data, ['user_id' => 'required|exists:users,id']);
+        if ($validator->fails()) { return redirect()->back()->withErrors($validator)->withInput();}
+
+        $tags['tags'] = request('tags', []);
+        $validator = Validator::make($tags, ['tags' => 'required|exists:tags,id']);
+        if ($validator->fails()) {return redirect()->back()->withErrors($validator)->withInput();}
+
         $attributes['active'] = 1;
         $attributes['user_id'] = auth()->id();
         $attributes['column_id'] = request('column_id');
         $attributes['order'] = request('order'.$attributes['column_id'], 0);
         $attributes['text'] = request('text'.$attributes['column_id']);
 
-        $tags['tags'] = request('tags', []);
-
         $task = Task::create($attributes);
-
-        $this->validateTagTaskCreate();
-
         $task->tags()->attach($tags['tags']);
 
         return redirect(route("home"));
@@ -59,12 +64,13 @@ class TasksController extends Controller
 
     protected function validateTaskCreate($column_id)
     {
-        return request()->validate([
+        return request()->validate(
+        [
             'text'.$column_id => ['required', 'max:255',],
             'order'.$column_id => 'required|numeric|min:0|max:100',
-            'column_id' => 'required|exists:columns,id',
-            'user_id' => 'required|exists:users,id'
-        ], [
+            'column_id' => 'required|exists:columns,id'
+        ],
+        [
             'text'.$column_id.'.required' => 'The task text is required.',
             'text'.$column_id.'.max' => 'The task text may not be greater than 255 characters.',
             'order'.$column_id.'.required' => 'The order is required.',
