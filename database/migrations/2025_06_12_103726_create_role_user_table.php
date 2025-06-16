91bb976ab6ddb8355c1097ec9fbfa1c20a7a1ec4
eneko-izt <eneko@izt.coop>
2025-06-16 14:59:08 +0200

    Added soft delete field to role_user migration. Updated chart adding roles and role_user tables

--- database/migrations/2025_06_12_103726_create_role_user_table.php
+++ database/migrations/2025_06_12_103726_create_role_user_table.php
@@ -25,6 +25,7 @@ class CreateRoleUserTable extends Migration
             $table->unique(['role_id', 'user_id']);
 
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
