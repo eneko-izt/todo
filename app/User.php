a91ce50bc61b08adf0c464ddb556c31ee5224ebb
eneko-izt <eneko@izt.coop>
2025-06-17 10:26:52 +0200

    Created user control. Added routes. Created user index view

--- app/User.php
+++ app/User.php
@@ -5,6 +5,9 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
 {
