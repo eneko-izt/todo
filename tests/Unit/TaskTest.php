cad07892a0f3abc23cbbe6f29156a8819a5ec571
eneko-izt <eneko@izt.coop>
2025-07-28 08:41:49 +0200

    Added indeoendent test functions rather than a single one with all test.

--- tests/Unit/TaskTest.php
+++ tests/Unit/TaskTest.php
@@ -11,14 +11,6 @@ class TaskTest extends TestCase
 {
     use RefreshDatabase;
 
-
-    private $transformer;
-    private $getItemList;
-    private $user;
-    private $column;
-    private $tag;
-    private $task;
-
     /**
      * Test Task validation: Run the test with vendor/bin/phpunit in attached shell
      * 
@@ -44,274 +36,264 @@ class TaskTest extends TestCase
      * 
      */
 
-    //TODO: test bat konprobaketa bat 
-    //7 gauza ezberdin testatzen ari gara hemen, kodearen mantentze-lanak zailtzen du
-    //Hobe da test bakoitza gauza bakar bat egiaztatzea, eta horrela mantentze-lanak errazagoak dira
-    //php vendor/bin/phpunit  --testdox oso zaila da jarkitea zer ari garen testeatzen
-    public function testExample()
+
+    public function testCreateEmptyTask()
     {
-        $role = factory(\App\Role::class)->create(['name' => 'user']);
-        $this->user = factory(\App\User::class)->create(['active' => 1]);
-        $this->user->roles()->attach($role);
-        $this->column = factory(\App\Column::class)->create(['active' => true, 'deleted_at' => null]);
+        $taskService = new \App\Http\Services\TaskService();
 
-        $this->tag = factory(\App\Tag::class)->create();
-
-        $this->task = factory(\App\Task::class)->create([
-            'user_id' => $this->user->id,
-            'column_id' => $this->column->id,
-            'active' => true,
-            'deleted_at' => null
-        ]);
-
-        // The next 2 checks also verify suffix mechanism works
-        $this->checkEmptyTaskCreate();
-        $this->checkEmptyTaskUpdate();
-
-        // check Text
-        $this->checkText();
-
-        // check Order
-        $this->checkOrder();
-
-        // check column id
-        $this->checkColumn();
-
-        // check tags
-        $this->checkTags();
-
-        // check user id
-        $this->checkUser();
-    }
-
-
-    private function setupControllerValidation($method)
-    {
-        //TODO: hau ez dut oso ondo ulertzen, zertarako da?
-        $this->transformer = new \App\Http\Controllers\TasksController();
-        $reflection = new ReflectionClass(get_class($this->transformer));
-        $this->getItemList = $reflection->getMethod($method);
-        $this->getItemList->setAccessible(true);
-    }
-
-    private function checkEmptyTaskCreate()
-    {
-        $this->setupControllerValidation('createValidator');
-
-        // When creating Column id is used to suffix fields with a unique identifier
-        // Therefore itself cannot be suffixed
-        $id = 111; // Not existing column so it should fail validation
-        //TODO: harkodeatutako id-ak ez dira oso adierazgarriak, zergatik ez da erabiltzen $nonExistingColumnId? (Bilatu azken id, eta hurrengoa erabili, adibidez)
+        // Not existing column id so it must fail validation
+        $task = $this->lastTask();
+        $nonExistingColumnId = $task ? $task->id + 1 : 1;
         $data = array(
-            'text' . $id => null,
-            'order' . $id => null,
-            'column_id' => $id,
-            'tags' . $id => [],
-            'user_id' . $id => null,
+            'text' . $nonExistingColumnId => null,
+            'order' . $nonExistingColumnId => null,
+            'column_id' => $nonExistingColumnId,
+            'tags' . $nonExistingColumnId => [],
+            'user_id' . $nonExistingColumnId => null,
             'active' => 1
         );
 
-        $err = $this->invokeValidate([$data]);
+        $validator = $taskService->createValidator($data);
+        $err = $validator->fails() ? $validator->errors()->keys() : [];
 
-        $res = count($err) == 4
-            && in_array("text" . $id, $err) && in_array("order" . $id, $err)
-            && in_array('column_id', $err) && in_array('user_id' . $id, $err);
+        $response = count($err) == 4
+            && in_array("text" . $nonExistingColumnId, $err) && in_array("order" . $nonExistingColumnId, $err)
+            && in_array('column_id', $err) && in_array('user_id' . $nonExistingColumnId, $err);
 
-        $this->assertTrue($res);
+        $this->assertTrue($response);
     }
 
-    private function checkEmptyTaskUpdate()
+    public function testUpdateEmptyTask()
     {
-        $this->setupControllerValidation('updateValidator');
+        $taskService = new \App\Http\Services\TaskService();
+        $this->fillDatabase();
 
-        // When updating Task id is used to suffix fields with a unique identifier
-        // When updating a task column id cannot has also to be suffixed with task_id
-        $id = $this->task->id;
+        $task = $this->lastTask();
+        $taskId = $task->id;
         $data = array(
-            'text' . $id => null,
-            'order' . $id => null,
-            'column_id' . $id => null,
-            'tags' . $id => [],
-            'user_id' . $id => null,
+            'text' . $taskId => null,
+            'order' . $taskId => null,
+            'column_id' . $taskId => null,
+            'tags' . $taskId => [],
+            'user_id' . $taskId => null,
             'active' => 1
         );
 
-        $err = $this->invokeValidate([$data, $this->task]);
+        $validator = $taskService->updateValidator($data, $task);
+        $err = $validator->fails() ? $validator->errors()->keys() : [];
 
-        $res = count($err) == 4
-            && in_array("text" . $id, $err) && in_array("order" . $id, $err)
-            && in_array("column_id" . $id, $err) && in_array('user_id' . $id, $err);
+        $response = count($err) == 4
+            && in_array("text" . $taskId, $err) && in_array("order" . $taskId, $err)
+            && in_array("column_id" . $taskId, $err) && in_array('user_id' . $taskId, $err);
 
-        $this->assertTrue($res);
+        $this->assertTrue($response);
     }
 
-    private function checkText()
+    public function testText()
     {
-        $this->setupControllerValidation('createValidator');
+        $taskService = new \App\Http\Services\TaskService();
+        $this->fillDatabase();
+        $column = $this->lastColumn();
 
-        // When creating Column id is used to suffix fields with a unique identifier
-        // Therefore itself cannot be suffixed
         $data = $this->fillCreateRequestData();
-        $id = $this->column->id;
+        $columnId = $column->id;
 
         // Check empty string is invalid
-        $data['text' . $id] = "";
-        $err = $this->invokeValidate([$data]);
-        $res = count($err) == 1 && in_array("text" . $id, $err);
-        $this->assertTrue($res);
+        $data['text' . $columnId] = "";
+
+        $validator = $taskService->createValidator($data);
+        $err = $validator->fails() ? $validator->errors()->keys() : [];
+        $response = count($err) == 1 && in_array("text" . $columnId, $err);
+        $this->assertTrue($response);
 
         // Check length > 255 is invalid
         $invalidString = "1234567890";
         $invalidString = str_repeat($invalidString, 26);
-        $data['text' . $id] = $invalidString;
-        $err = $this->invokeValidate([$data]);
-        $res = count($err) == 1 && in_array("text" . $id, $err);
-        $this->assertTrue($res);
+        $data['text' . $columnId] = $invalidString;
+
+        $validator = $taskService->createValidator($data);
+        $err = $validator->fails() ? $validator->errors()->keys() : [];
+        $response = count($err) == 1 && in_array("text" . $columnId, $err);
+        $this->assertTrue($response);
 
         // Check a valid string
-        $data['text' . $id] = "abcd";
-        $err = $this->invokeValidate([$data]);
-        $res = count($err) == 0;
-        $this->assertTrue($res);
+        $data['text' . $columnId] = "abcd";
+        $validator = $taskService->createValidator($data);
+        $err = $validator->fails() ? $validator->errors()->keys() : [];
+        $response = count($err) == 0;
+        $this->assertTrue($response);
     }
 
-    private function checkOrder()
+    public function testOrder()
     {
-        $this->setupControllerValidation('createValidator');
+        $taskService = new \App\Http\Services\TaskService();
+        $this->fillDatabase();
+        $column = $this->lastColumn();
 
-        // When creating Column id is used to suffix fields with a unique identifier
-        // Therefore itself cannot be suffixed
         $data = $this->fillCreateRequestData();
-        $id = $this->column->id;
+        $columnId = $column->id;
 
         // Check order must be numeric
-        $data['order' . $id] = "abcd";
-        $err = $this->invokeValidate([$data]);
-        //TODO: aldagaien izenak ez dira oso adierazgarriak, zer da $res? $response jarrita errazagoa da kodea ultertzea.
-        $res = count($err) == 1 && in_array("order" . $id, $err);
-        $this->assertTrue($res);
+        $data['order' . $columnId] = "abcd";
+        $validator = $taskService->createValidator($data);
+        $err = $validator->fails() ? $validator->errors()->keys() : [];
+        $response = count($err) == 1 && in_array("order" . $columnId, $err);
+        $this->assertTrue($response);
 
         // Check order cannot be negative
-        $data['order' . $id] = -1;
-        $err = $this->invokeValidate([$data]);
-        $res = count($err) == 1 && in_array("order" . $id, $err);
-        $this->assertTrue($res);
+        $data['order' . $columnId] = -1;
+        $validator = $taskService->createValidator($data);
+        $err = $validator->fails() ? $validator->errors()->keys() : [];
+        $response = count($err) == 1 && in_array("order" . $columnId, $err);
+        $this->assertTrue($response);
 
         // Check order cannot be greater than 100
-        $data['order' . $id] = 101;
-        $err = $this->invokeValidate([$data]);
-        $res = count($err) == 1 && in_array("order" . $id, $err);
-        $this->assertTrue($res);
+        $data['order' . $columnId] = 101;
+        $validator = $taskService->createValidator($data);
+        $err = $validator->fails() ? $validator->errors()->keys() : [];
+        $response = count($err) == 1 && in_array("order" . $columnId, $err);
+        $this->assertTrue($response);
 
         // Check valid order
-        $data['order' . $id] = 1;
-        $err = $this->invokeValidate([$data]);
-        $res = count($err) == 0;
-        $this->assertTrue($res);
+        $data['order' . $columnId] = 1;
+        $validator = $taskService->createValidator($data);
+        $err = $validator->fails() ? $validator->errors()->keys() : [];
+        $response = count($err) == 0;
+        $this->assertTrue($response);
     }
 
-    private function checkColumn()
+    public function testColumn()
     {
-        $this->setupControllerValidation('createValidator');
-
-        // When creating Column id is used to suffix fields with a unique identifier
-        // Therefore itself cannot be suffixed
-        $columnId = $this->column->id;
+        $taskService = new \App\Http\Services\TaskService();
+        $this->fillDatabase();
 
         $data = $this->fillCreateRequestData();
-        $id = $this->column->id;
 
         // Check column cannot be null
-        $this->column->id = null;
-        $data = $this->fillCreateRequestData();
-        $err = $this->invokeValidate([$data]);
-        $res = count($err) == 1;
-        $this->assertTrue($res);
+        $data['column_id'] = null;
+        $validator = $taskService->createValidator($data);
+        $err = $validator->fails() ? $validator->errors()->keys() : [];
+        $response = count($err) == 4
+            && in_array("text", $err) && in_array("order", $err)
+            && in_array("column_id", $err) && in_array('user_id', $err);
+        $this->assertTrue($response);
 
         // Check column must exist in table
-        $this->column->id = 3662;
-        $data = $this->fillCreateRequestData();
-        $err = $this->invokeValidate([$data]);
-        $res = count($err) == 1 && in_array("column_id", $err);
-        $this->assertTrue($res);
-
-        $this->column->id = $columnId;
+        $nonExistingColumnId = $this->lastColumn()->id + 1;
+        $data = $this->fillCreateRequestData($nonExistingColumnId);
+        $data['column_id'] = $nonExistingColumnId;
+        $validator = $taskService->createValidator($data);
+        $err = $validator->fails() ? $validator->errors()->keys() : [];
+        $response = count($err) == 1 && in_array("column_id", $err);
+        $this->assertTrue($response);
     }
 
-    private function checkTags()
+    public function testTags()
     {
-        $this->setupControllerValidation('createValidator');
+        $taskService = new \App\Http\Services\TaskService();
+        $this->fillDatabase();
 
-        // When creating Column id is used to suffix fields with a unique identifier
-        // Therefore itself cannot be suffixed
         $data = $this->fillCreateRequestData();
-        $id = $this->column->id;
+        $columnId = $this->lastColumn()->id;
 
         // Check tags validation succeeds when tags not provided
-        $data['tags' . $id] = [];
-        $err = $this->invokeValidate([$data]);
-        $res = count($err) == 0;
-        $this->assertTrue($res);
+        $data['tags' . $columnId] = [];
+        $validator = $taskService->createValidator($data);
+        $err = $validator->fails() ? $validator->errors()->keys() : [];
+        $response = count($err) == 0;
+        $this->assertTrue($response);
 
         // Check tags validation succeeds with an existing tag
-        $data['tags' . $id] = [$this->tag->id];
-        $err = $this->invokeValidate([$data]);
-        $res = count($err) == 0;
-        $this->assertTrue($res);
+        $data['tags' . $columnId] = [$this->lastTag()->id];
+        $validator = $taskService->createValidator($data);
+        $err = $validator->fails() ? $validator->errors()->keys() : [];
+        $response = count($err) == 0;
+        $this->assertTrue($response);
 
         // Check tags validation fails with a not existing tag id
-        $data['tags' . $id] = [5335];
-        $err = $this->invokeValidate([$data]);
-        $res = count($err) == 1 && in_array("tags" . $id, $err);
-        $this->assertTrue($res);
+        $data['tags' . $columnId] = [$this->lastTag()->id + 1];
+        $validator = $taskService->createValidator($data);
+        $err = $validator->fails() ? $validator->errors()->keys() : [];
+        $response = count($err) == 1 && in_array("tags" . $columnId, $err);
+        $this->assertTrue($response);
     }
 
-    private function checkUser()
+    public function testUser()
     {
-        $this->setupControllerValidation('createValidator');
+        $taskService = new \App\Http\Services\TaskService();
+        $this->fillDatabase();
 
-        // When creating Column id is used to suffix fields with a unique identifier
-        // Therefore itself cannot be suffixed
         $data = $this->fillCreateRequestData();
-        $id = $this->column->id;
+        $columnId = $this->lastColumn()->id;
 
         // Check validation succeeds for existing user
-        $data['user_id' . $id] = $this->user->id;
-        $err = $this->invokeValidate([$data]);
-        $res = count($err) == 0;
-        $this->assertTrue($res);
+        $data['user_id' . $columnId] = $this->lastUser()->id;
+        $validator = $taskService->createValidator($data);
+        $err = $validator->fails() ? $validator->errors()->keys() : [];
+        $response = count($err) == 0;
+        $this->assertTrue($response);
 
         // create a new user model instance BUT do not save it in the database!
+        // validation should fail
         $user = factory(\App\User::class)->make(['active' => 1]);
         $this->actingAs($user);
-        $data['user_id' . $id] = $user->id;
-        $err = $this->invokeValidate([$data]);
-        $res = count($err) == 1 && in_array("user_id" . $id, $err);
-        $this->assertTrue($res);
+        $data['user_id' . $columnId] = $user->id;
+        $validator = $taskService->createValidator($data);
+        $err = $validator->fails() ? $validator->errors()->keys() : [];
+        $response = count($err) == 1 && in_array("user_id" . $columnId, $err);
+        $this->assertTrue($response);
     }
 
-    private function invokeValidate($params)
+    private function fillDatabase()
     {
-        $err = [];
-        $validator = $this->getItemList->invokeArgs($this->transformer, $params);
-        if ($validator->fails()) {
-            $err = $validator->errors()->keys();
-        }
-        return $err;
+        $role = factory(\App\Role::class)->create(['name' => 'user']);
+        $user = factory(\App\User::class)->create(['active' => 1]);
+        $user->roles()->attach($role);
+        $column = factory(\App\Column::class)->create(['active' => true, 'deleted_at' => null]);
+
+        $tag = factory(\App\Tag::class)->create(['active' => true, 'deleted_at' => null]);
+
+        $task = factory(\App\Task::class)->create([
+            'user_id' => $user->id,
+            'column_id' => $column->id,
+            'active' => true,
+            'deleted_at' => null
+        ]);
     }
 
-    private function fillCreateRequestData()
+    private function fillCreateRequestData($suffix = null)
     {
-        $id = $this->column->id;
+        $suffix = $suffix ?? $this->lastColumn()->id;
+
         $data = array(
-            'text' . $id => 'abcdef',
-            'order' . $id => 1,
-            'column_id' => $id,
-            'tags' . $id => [$this->tag->id],
-            'user_id' . $id => $this->user->id,
+            'text' . $suffix => 'abcdef',
+            'order' . $suffix => 1,
+            'column_id' => $suffix,
+            'tags' . $suffix => [$this->lastTag()->id],
+            'user_id' . $suffix => $this->lastUser()->id,
             'active' => 1
         );
 
         return $data;
+    }
+
+    private function lastColumn()
+    {
+        return \App\Column::all()->last();
+    }
+
+    private function lastTag()
+    {
+        return \App\Tag::all()->last();
+    }
+
+    private function lastUser()
+    {
+        return \App\User::all()->last();
+    }
+
+    private function lastTask()
+    {
+        return \App\Task::all()->last();
     }
 }
