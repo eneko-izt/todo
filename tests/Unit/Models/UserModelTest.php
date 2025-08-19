7f11221f78f4b654384e3d875490df28403df3b1
eneko-izt <eneko@izt.coop>
2025-08-19 11:10:07 +0200

    Changed reviewed tests

--- tests/Unit/Models/UserModelTest.php
+++ tests/Unit/Models/UserModelTest.php
@@ -132,8 +132,8 @@ class UserModelTest extends TestCase
         // Attach unused role to unused user
         $notUsedUser->roles()->attach($notUsedRole->id);
 
+        $this->assertCount(1, $user->roles);
         $this->assertTrue($user->roles->contains($role));
-        $this->assertInstanceOf(Role::class, $user->roles->first());
     }
 
     public function test_it_has_many_roles()
@@ -148,9 +148,117 @@ class UserModelTest extends TestCase
         $user->roles()->attach($role2->id);
         $user->roles()->attach($role3->id);
 
-        $roles = collect([$role1, $role2, $role3]);
-        $arraysAreEqual = empty(array_diff($roles->pluck('id')->toArray(), $user->roles->pluck('id')->toArray()))
-            && empty(array_diff($user->roles->pluck('id')->toArray(), $roles->pluck('id')->toArray()));
-        $this->assertTrue($arraysAreEqual);
+        $this->assertCount(3, $user->roles);
+        $this->assertTrue($user->roles->contains($role1));
+        $this->assertTrue($user->roles->contains($role2));
+        $this->assertTrue($user->roles->contains($role3));
+    }
+
+    public function test_rolesWithTrashed_none_deleted()
+    {
+        $user = factory(User::class)->create();
+        $roleNotDeleted1 = factory(Role::class)->create(['deleted_at' => null]);
+        $roleNotDeleted2 = factory(Role::class)->create(['deleted_at' => null]);
+
+        $user->roles()->attach($roleNotDeleted1->id);
+        $user->roles()->attach($roleNotDeleted2->id);
+
+        $this->assertTrue($user->roles->contains($roleNotDeleted1));
+        $this->assertTrue($user->roles->contains($roleNotDeleted2));
+        $this->assertTrue($user->roles->count() == $user->rolesWithTrashed->count());
+    }
+
+    public function test_rolesWithTrashed_one_not_deleted_one_deleted()
+    {
+        $user = factory(User::class)->create();
+        $roleNotDeleted = factory(Role::class)->create(['deleted_at' => null]);
+        $roleDeleted = factory(Role::class)->create(['deleted_at' => null]);
+
+        $user->roles()->attach($roleNotDeleted->id);
+        $user->roles()->attach($roleDeleted->id);
+
+        $user->roles()->updateExistingPivot($roleDeleted->id, ['deleted_at' => now()]);
+
+        $this->assertTrue($user->roles->contains($roleNotDeleted));
+        $this->assertFalse($user->roles->contains($roleDeleted));
+
+        $this->assertTrue($user->rolesWithTrashed->contains($roleNotDeleted));
+        $this->assertTrue($user->rolesWithTrashed->contains($roleDeleted));
+    }
+
+    public function test_rolesWithTrashed_one_not_deleted_many_deleted()
+    {
+        $user = factory(User::class)->create();
+        $roleNotDeleted = factory(Role::class)->create(['deleted_at' => null]);
+        $roleDeleted1 = factory(Role::class)->create(['deleted_at' => null]);
+        $roleDeleted2 = factory(Role::class)->create(['deleted_at' => null]);
+        $roleDeleted3 = factory(Role::class)->create(['deleted_at' => null]);
+
+        $user->roles()->attach($roleNotDeleted->id);
+        $user->roles()->attach($roleDeleted1->id);
+        $user->roles()->attach($roleDeleted2->id);
+        $user->roles()->attach($roleDeleted3->id);
+
+        $user->roles()->updateExistingPivot($roleDeleted1->id, ['deleted_at' => now()]);
+        $user->roles()->updateExistingPivot($roleDeleted2->id, ['deleted_at' => now()]);
+        $user->roles()->updateExistingPivot($roleDeleted3->id, ['deleted_at' => now()]);
+
+        $this->assertTrue($user->roles->contains($roleNotDeleted));
+        $this->assertFalse($user->roles->contains($roleDeleted1));
+        $this->assertFalse($user->roles->contains($roleDeleted2));
+        $this->assertFalse($user->roles->contains($roleDeleted3));
+
+        $this->assertTrue($user->rolesWithTrashed->contains($roleNotDeleted));
+        $this->assertTrue($user->rolesWithTrashed->contains($roleDeleted1));
+        $this->assertTrue($user->rolesWithTrashed->contains($roleDeleted2));
+        $this->assertTrue($user->rolesWithTrashed->contains($roleDeleted3));
+    }
+
+    public function test_role_id_has_none()
+    {
+        $user = factory(User::class)->create();
+        $role = factory(Role::class)->create(['deleted_at' => null]);
+
+        $this->assertFalse($user->hasRoleId($role->id));
+    }
+
+    public function test_role_id_has_one()
+    {
+        $user = factory(User::class)->create();
+        $role = factory(Role::class)->create(['deleted_at' => null]);
+
+        $user->roles()->attach($role->id);
+
+        $this->assertTrue($user->hasRoleId($role->id));
+    }
+
+    public function test_role_id_has_many()
+    {
+        $user = factory(User::class)->create();
+        $role1 = factory(Role::class)->create(['deleted_at' => null]);
+        $role2 = factory(Role::class)->create(['deleted_at' => null]);
+        $role3 = factory(Role::class)->create(['deleted_at' => null]);
+
+        $user->roles()->attach($role1->id);
+        $user->roles()->attach($role2->id);
+        $user->roles()->attach($role3->id);
+
+        $this->assertTrue($user->hasRoleId($role1->id));
+        $this->assertTrue($user->hasRoleId($role2->id));
+        $this->assertTrue($user->hasRoleId($role3->id));
+    }
+
+    public function test_role_id_has_not_any_deleted()
+    {
+        $user = factory(User::class)->create();
+        $roleNotDeleted = factory(Role::class)->create(['deleted_at' => null]);
+        $roleDeleted = factory(Role::class)->create(['deleted_at' => null]);
+
+        $user->roles()->attach($roleNotDeleted->id);
+        $user->roles()->attach($roleDeleted->id);
+        $user->roles()->updateExistingPivot($roleDeleted->id, ['deleted_at' => now()]);
+
+        $this->assertTrue($user->hasRoleId($roleNotDeleted->id));
+        $this->assertFalse($user->hasRoleId($roleDeleted->id));
     }
 }
