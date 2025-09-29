d89c6b7f092f27bf0202973a3fc46943a9ef48f6
eneko-izt <eneko@izt.coop>
2025-09-29 15:30:19 +0200

    Fixed column colour

--- resources/views/home2.blade.php
+++ resources/views/home2.blade.php
@@ -4,7 +4,7 @@
 
     @forelse ($columns as $column)
 
-        <div class="w-80 rounded-xl p-4 bg-blue-100">
+        <div class="w-80 rounded-xl p-4" style="background-color: {{ $column->colour }};">
             <div class="flex justify-between items-center mb-3">
                 <h2 class="font-bold">{{ $column->name }}</h2>
                 <button class="bg-blue-500 text-white text-sm px-2 py-1 rounded">+ Task</button>
